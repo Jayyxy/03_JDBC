7c27cbd18fde76567c0773a26df32d767189cbdd
Jayyxy <mimichoi0121@gmail.com>
2025-03-17 17:34:24 +0900

    JDBC first

--- jdbc1/src/edu/kh/jdbc/JDBCExample6.java
+++ jdbc1/src/edu/kh/jdbc/JDBCExample6.java
@@ -1,5 +1,19 @@
 package edu.kh.jdbc;
 
 public class JDBCExample6 {
-
+	
+	
+	public static void main(String[] args) {
+	
+		
+		// 아이디, 비밀번호, 이름을 입력받아
+		// 아이디, 비밀번호가 일치하는 사용자의 
+		// 이름을 수정(UPDATE) 
+		
+		// 1. PreparedStatement 이용하기
+		// 2. commit/rollback 처리하기
+		// 3. 성공 시 "수정 성공!" 출력 / 실패 시 "아아디 또는 비밀번호 불일치" 출력
+		
+		
+}
 }
