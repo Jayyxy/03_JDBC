a374ce4c600a71c148bc0d2a7871eac75b44c396
Jayyxy <mimichoi0121@gmail.com>
2025-03-18 17:52:18 +0900

    dfs

--- jdbc.2/src/edu/kh/jdbc/dao/UserDAO.java
+++ jdbc.2/src/edu/kh/jdbc/dao/UserDAO.java
@@ -1,5 +1,90 @@
 package edu.kh.jdbc.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import edu.kh.jdbc.common.JDBCTemplate;
+import edu.kh.jdbc.dto.User;
+
+// (Model 중 하나)DAO (Data Access Object)
+// 데이터가 저장된 곳에 접근하는 용도의 객체
+// -> DB 에 접근하여 Java에서 원하는 결과를 얻기위해
+//	SQL 을 수행하고 결과를 반환 받는 역할
+
 public class UserDAO {
 
+	
+	// 필드
+	// - DB 접근 관련한 JDBC 객체 참조 변수 미리 선언
+	private Statement stmt = null;
+	private PreparedStatement pstmt = null;
+	private ResultSet rs = null;
+	public User selectId(Connection conn, String input) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+	// 메서드
+	/* 전달받은 Connection을 이용해서 DB에 접근하여
+	 * 전달받은 아이디(input)와 일치하는 User 정보를 DB 조회하기
+	 * @param conn : Service 에서 생성한 Connection 객체
+	 * @param input : View 에 입력받은 아이디
+	 * @return 아이디가 일치하는 회원의 User 또는 null
+	 * 
+	 * */
+	public User selectId(Connection conn, String input) {
+		
+		// 1. 결과 저장용 변수 선언
+		User user = null;
+		try {
+			
+			// SQL 작성
+			
+			String sql = "SELECT * FROM TB_USER WHERE USER_ID = ? "
+			
+			// 3. PreparedStatement 객체 생성
+			pstmt =conn.prepareStatement(sql);
+			
+			// 4. ? (위치홀더) 에 알맞은 값 세팅
+			pstmt.setString(1,input);
+			
+			// 5. SQL 수행 후 결과 반환 받기
+			rs = pstmt.executeQuery();
+			
+			// 6. 조회 결과가 있을 경우
+			// 중복되는 아이디가 없다고 가정
+			// -> 1행만 조회되기 때문에 while문 보다는 if를 사용하는게 효과적
+			if(rs.next()) {
+				// 첫 행에 데이터가 존재한다면
+				
+				// 각 컬럼의 값 얻어오기
+				int userNo = rs.getInt("USER_ID");
+				String userId = rs.getString("USER_ID");
+				String userPw = rs.getString("USER_PW");
+				String userName = rs.getString("USER_NAME");
+				// java.sql.Date
+				Date enrollDate = rs.getDate("ENROLL_DATE");
+				
+				// 조회된 컬럼값 
+				
+			}
+			
+		}catch(Exception e){
+		
+			e.printStackTrace()
+		
+		}finally {
+			
+			// 사용자 JDBC 객체 자원  반환 close
+			JDBCTemplate.close(rs);
+			JDBCTemplate.close(pstmt);
+		}
+		
+		
+		return user; // 결과 반환 (생성된 User 객체 또는 null)
+		
+	}
 }
--- jdbc.2/src/edu/kh/jdbc/dto/User.java
+++ jdbc.2/src/edu/kh/jdbc/dto/User.java
@@ -1,5 +1,45 @@
 package edu.kh.jdbc.dto;
 
 public class User {
+	
+// DTO (Data Transfer Object - 데이터 전송 객체)
+// : 값을 묶어서 전달하는 용도의 객체
+// -> DB에 데이터를 묶어서 전달하거나
+// DB에서 조회한 결과를 가져올 때 사용 (데이터 교한을 위한 객체)
+// == DB 특정 테이블의 한 행의 데이터를
+// 저장할 수 있는 형태로 class 작성
 
+// lombok : 자바 코드에서 반복적으로 작성해야하는 코드 (보일럴 플 뭐시기)
+// 자동으로 완성해주는 라이브러리
+
+@Getter
+@Setter 
+@NoArgsConstructor
+@AllArgsConstructor 
+@ToString 
+public class user{
+	private int userNo;
+	private String userId;
+	private String userPw;
+	private String userName;
+	private String enrollDate;
+	// -> enrollDate를 java.sql.Date 이 아닌 String 으로 한 이유
+	// -> DB 조회 시 날짜 데이터를 원하는 형태의 문자열로
+	// 변환하여 조회할 예정 -> TO_CHAR() 이용
+	
+	
+	
+	// getter/setter
+	// 생성자들
+	//
+	
+	
+	
+	
+	
+	
+	
+}
+	
+	
 }
